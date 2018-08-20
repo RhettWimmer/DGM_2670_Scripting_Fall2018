3f4618a3996065c98a7057ca6f4ba37e25b0540e
Rhett <thedovahbrine@gmail.com>
2018-08-20 14:00:13 -0600

    Added script

---
+++ 2670/Assets/IfStatements.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (true)
+		{
+			//do work
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
