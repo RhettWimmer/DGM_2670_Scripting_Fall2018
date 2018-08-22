6da713129f87b2c650f9d0332aaea0cdb96f7617
Rhett <thedovahbrine@gmail.com>
2018-08-22 14:24:31 -0600

    Added Sprite Test

---
+++ Sprite Tests/Assets/PrefabData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class PrefabData : ScriptableObject
+{
+
+    public GameObject Prefab;
+    public Sprite SpriteImage;
+    public Color SpriteColor;
+
+    public void OnConfigure()
+    {
+        var newPrefab = Instantiate(Prefab);
+        var newSprite = newPrefab.GetComponent<SpriteRenderer>();
+        newSprite.sprite = SpriteImage;
+        newSprite.color = SpriteColor;
+    }
+}
