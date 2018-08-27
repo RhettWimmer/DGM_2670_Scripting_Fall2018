2276e40f95fd8cd3f6908c1bbc91159c07183ca8
Rhett <thedovahbrine@gmail.com>
2018-08-27 14:36:43 -0600

    Leaving Class, finished onclick particles

---
+++ Sprite Tests/Assets/EventScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventScript : MonoBehaviour
+{
+
+	public UnityEvent Event;
+
+	private void OnMouseDown()
+	{
+		Event.Invoke();
+	}
+
+	/*
+	 private void Start()
+	
+	{
+		MyEvent.Invoke();
+	}
+	*/
+	private void OnTriggerEnter(Collider other)
+	{
+		Event.Invoke();
+	}
+}
