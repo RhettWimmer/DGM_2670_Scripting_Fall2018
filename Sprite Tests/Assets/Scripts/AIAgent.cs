7d65604294a1dee33d75d41fa00e12d1addf3c51
Rhett <thedovahbrine@gmail.com>
2018-09-17 14:26:08 -0600

    Worked with navmesh

---
+++ Sprite Tests/Assets/Scripts/AIAgent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent((typeof(NavMeshAgent)))]
+public class AIAgent : MonoBehaviour
+{
+
+	private NavMeshAgent agent;
+	public Transform Target;
+//	public bool CanChase;
+	public Transform PostPoint;
+	private Transform finalDestination;
+	
+	private void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		finalDestination = transform;
+	}
+
+	private void OnTriggerEnter(Collider obj)
+	{
+		if (obj.transform == Target)
+		finalDestination = Target;
+	}
+
+	private void OnTriggerExit(Collider obj)
+	{
+		finalDestination = PostPoint;
+	}
+
+	private void Update()
+	{
+		agent.destination = finalDestination.position;
+
+	}
+}
