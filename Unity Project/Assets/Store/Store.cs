0e0dd6dbc7d4d0d2dc4d932beffd1e76c943a77b
Rhett <thedovahbrine@gmail.com>
2018-10-15 14:28:10 -0600

    Starting prototyping

---
+++ Unity Project/Assets/Store/Store.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Store", menuName = "Store/StoreFront")]
+public class Store : ScriptableObject
+{
+
+	public Objects Available;
+	public Objects Purchased;
+
+	public void MakePurchase(Object obj)
+	{
+		foreach (var availableObject in Available.ObjectList)
+		{
+			if (availableObject == obj)
+			{
+				Purchased.ObjectList.Add(obj);
+				Available.ObjectList.Remove(availableObject);
+			}
+		}
+	}
+}
