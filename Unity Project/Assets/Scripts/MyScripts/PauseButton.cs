6f11565d972d2fe099824281c25d14181ceb96c4
Rhett <thedovahbrine@gmail.com>
2018-10-17 02:01:25 -0600

    Adding Pause menu, since I like UI

---
+++ Unity Project/Assets/Scripts/MyScripts/PauseButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour
+{
+
+	void Start()
+	{
+		Time.timeScale = 1;
+
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+			}
+			else if (Time.timeScale == 0)
+			{
+				Time.timeScale = 1;
+			}
+		}
+	}
+}
---
+++ Unity Project/Assets/Scripts/MyScripts/PauseOnClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;  
+
+public class PauseOnClick : MonoBehaviour{
+	
+	void Start(){
+	GetComponent<Button>().onClick.AddListener(Pause);
+}
+	public void Pause()
+	{
+		Time.timeScale = 0;
+		Debug.Log("Is Paused");
+	}
+}
