4c2e16d2353695dd9884e3d4425916cc310cde0b
Rhett <thedovahbrine@gmail.com>
2018-10-22 14:35:29 -0600

    Starting prototyping

---
+++ Unity Project/Assets/Scripts/MyScripts/SpawnRand.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRand : MonoBehaviour
+{
+
+    public GameObject SpawnOne;
+    public GameObject SpawnTwo;
+    public GameObject SpawnThree;
+    public GameObject Enemy;
+
+    private void Start()
+    {
+        Instantiate(Enemy);
+    }
+}
