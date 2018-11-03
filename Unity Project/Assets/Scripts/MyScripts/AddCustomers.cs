11f7c04e577f362ffd533d0fd0ed2d48d90589cf
Rhett <thedovahbrine@gmail.com>
2018-11-03 05:29:19 -0600

    Worked on customer spawner

---
+++ Unity Project/Assets/Scripts/MyScripts/AddCustomers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCustomers : MonoBehaviour
+{
+
+    public GameObject Customer;
+    public float SpawnTimer = 2f;
+
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", SpawnTimer, SpawnTimer);
+    }
+
+    private void Spawn()
+    {
+        Instantiate(Customer);
+    }
+}
