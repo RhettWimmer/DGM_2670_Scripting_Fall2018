82971ffa9bed9ccd57a0d43b66183732f8b947f3
Rhett <thedovahbrine@gmail.com>
2018-12-04 23:54:30 -0700

    Okay I'm going to die

---
+++ Unity Project/Assets/Scripts/MyScripts/IntData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewInt", menuName = "IntData")]
+public class IntData : ScriptableObject
+{
+
+    public int IntValue;
+
+    public virtual int Value
+    {
+        get { return IntValue; }
+    }
+}
--- Unity Project/Assets/Scripts/MyScripts/Money.cs
+++ Unity Project/Assets/Scripts/MyScripts/Money.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Money : MonoBehaviour {
+public class Money : MonoBehaviour
+{
 
+	public float Speed = 100;
+	public float Cash = 0;
 
 	void Update () {
-		transform.Rotate(Vector3.right * Time.deltaTime);		
+		transform.Rotate(Vector3.up, Speed * Time.deltaTime);		
+	}
+
+	//This Adds Money
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.name == "Clerk")
+		{	
+			Cash++;
+			Debug.Log("working" + Cash);
+		}
 	}
 }
