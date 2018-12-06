f1a0134529dce41a446fce8c5c297c49594cd1bb
Rhett <thedovahbrine@gmail.com>
2018-12-05 17:00:26 -0700

    reeee

---
+++ Unity Project/Assets/Scripts/MyScripts/ContinueGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueGame : MonoBehaviour {
+
+    public bool gameIsPaused;
+    public GameObject pauseMenu;
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                ContinueGame();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    
+    public void ContinueGame(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        gameIsPaused = false;
+
+    }
+    
+}
---
+++ Unity Project/Assets/Scripts/MyScripts/RestartButt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButt : MonoBehaviour {
+
+	public int RestartLevel;
+
+	public void LoadLevel(){
+		SceneManager.LoadScene(RestartLevel);
+		Time.timeScale = 1;
+	}	
+}
